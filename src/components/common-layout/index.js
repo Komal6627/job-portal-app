d6d6f4a2670b9244b24d6f7c8a24c23da61ee421
Komal Killedar <sunny.r@iworktech.com>
2025-08-07 19:22:25 +0530

    make changes in home component, create loading component, create commong layout

---
+++ src/components/common-layout/index.js
@@ -0,0 +1,14 @@
+function CommonLayout({children}) {
+    return(
+        <div  className="mx-auto max-w-7xl p-6 lg:px-8">
+            {/* Header Component */}
+
+
+
+            {/* Main Content */}
+            <main>{children}</main>
+        </div>
+    )
+}
+
+export default CommonLayout
