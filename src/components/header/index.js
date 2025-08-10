4b81a046736ebf75e555b78e583a67ec70d76a3f
Komal Killedar <sunny.r@iworktech.com>
2025-08-10 11:20:56 +0530

    add some css in header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -30,23 +30,26 @@ function Header() {
         <header className="flex h-16 w-full shrink-0 items-center">
             <Sheet>
                 <SheetTrigger asChild>
-                    <Button className="lg:hidden">
+                    <Button className="lg:hidden bg-violet-700  hover:text-indigo-800">
                         <AlignJustify className="h-6 w-6" />
                         <span className="sr-only">Toggle Navigation Menu</span>
                     </Button>
                 </SheetTrigger>
                 <SheetContent side="left">
-
-                    <Link className="mr-6 hidden lg:flex" href={'#'}>
+                    <Link className="text-violet-700 hover:text-indigo-800 mr-6 hidden lg:flex" href={'#'}>
                         <h3>JOBSCO</h3>
                     </Link>
+
+
+
+
                     <div className="grid gap-2 py-6">
                         {menuItems.map((menuItem) =>
                             menuItem.show ? (
                                 <Link
                                     key={menuItem.path}
                                     href={menuItem.path}
-                                    className="flex w-full items-center py-2 text-lg font-semibold"
+                                    className="flex w-full items-center py-2 text-lg font-semibold text-violet-700 hover:text-indigo-800"
                                 >
                                     {menuItem.label}
                                 </Link>
@@ -56,7 +59,15 @@ function Header() {
 
                 </SheetContent>
             </Sheet>
-            <Link className="hidden lg:flex mr-6" href={'/'}>JOBSCO</Link>
+            <Link className="text-violet-700 font-bold text-2xl hidden lg:flex mr-6" href={'/'}>JOBSCO</Link>
+            <nav className="ml-auto hidden lg:flex gap-6">
+                {
+                    menuItems.map((menuItem) => menuItem.show ?
+                        <Link key={menuItem.path} href={menuItem.path} className="group inline-flex h-9 w-max items-center rounded-md bg-white px-4 py-2 text-sm font-medium  text-violet-700 hover:text-indigo-800">
+                            {menuItem.label}
+                        </Link> : null)
+                }
+            </nav>
         </header>
     </div>
 }
