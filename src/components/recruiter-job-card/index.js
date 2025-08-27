2dfa0fa3c3e15f1a0da1c8149ee804c20290b23b
Komal6627 <komalkilledar1996@gmail.com>
2025-08-27 23:10:44 +0530

    job-icon  component completed

--- src/components/recruiter-job-card/index.js
+++ src/components/recruiter-job-card/index.js
@@ -1,11 +1,14 @@
 'use client'
 
+import CommonCard from "../common-card"
+
 function RecruiterJobCard({jobItem}) {
     return (
         <div>
-
+            <CommonCard/>
         </div>
     )
 }
 
 export default RecruiterJobCard
+
