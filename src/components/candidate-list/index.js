881e1cebf7a40307d0feb32994862d4777eab750
Komal6627 <komalkilledar1996@gmail.com>
2025-09-11 11:17:38 +0530

    add the disable property on button if there is no applicant

--- src/components/candidate-list/index.js
+++ src/components/candidate-list/index.js
@@ -1,5 +1,7 @@
 'use client'
 
+import { Button } from "../ui/button"
+
  
 function CandidateList({jobApplications, currentCandidateDetails, setCurrentCandidateDetails, showCurrentCandidateDetailsModal, setShowCurrentCandidateDetailsModal}) {
     return (
@@ -7,9 +9,10 @@ function CandidateList({jobApplications, currentCandidateDetails, setCurrentCand
             <div className="grid grid-cols-1 gap-3 p-10 md:grid-cols-2 lg:grid-cols-3">
                 {
                 jobApplications && jobApplications.length > 0 ? 
-                 jobApplications.map(jobApplicantItem => <div className="bg-white shadow-lg w-full max-w-sm rounded-lg overflow-hidden mx-auto mt-4">
+                 jobApplications.map(jobApplicantItem => <div    key={jobApplicantItem._id || jobApplicantItem.id}  className="bg-white shadow-lg w-full max-w-sm rounded-lg overflow-hidden mx-auto mt-4">
                     <div className="px-4 my-6 flex justify-between items-center">
                         <h3>{jobApplicantItem?.name}</h3>
+                        <Button className="disabled:opacity-60 flex h-11 items-center justify-center px-5 bg-violet-600 text:white hover:bg-violet-700">View Profile</Button>
                     </div>
                  </div>) : null
                 }
--- src/components/recruiter-job-card/index.js
+++ src/components/recruiter-job-card/index.js
@@ -1,46 +1,63 @@
-'use client'
+"use client";
 
 import { useState } from "react";
-import CommonCard from "../common-card"
-import JobIcon from "../job-icon"
-import { Button } from "../ui/button"
+import CommonCard from "../common-card";
+import JobIcon from "../job-icon";
+import { Button } from "../ui/button";
 import JobApplications from "../job-applicants";
 
-function RecruiterJobCard({jobItem, jobApplications}) {
-    console.log(jobApplications, "jobApplications for recruiter");
-    
-    const [showApplicantsDrawer, setShowApplicantsDrawer] = useState(false)
-    const[currentCandidateDetails, setCurrentCandidateDetails] = useState(null)
-    const [showCurrentCandidateDetailsModal, setShowCurrentCandidateDetailsModal] = useState(false)
+function RecruiterJobCard({ jobItem, jobApplications }) {
+  console.log(jobApplications, "jobApplications for recruiter");
 
-     const applicantCount = jobApplications.filter(
-    (item) => item.jobId === jobItem?._id.toString()  
+  const [showApplicantsDrawer, setShowApplicantsDrawer] = useState(false);
+  const [currentCandidateDetails, setCurrentCandidateDetails] = useState(null);
+  const [
+    showCurrentCandidateDetailsModal,
+    setShowCurrentCandidateDetailsModal,
+  ] = useState(false);
+
+  const applicantCount = jobApplications.filter(
+    (item) => item.jobId === jobItem?._id.toString()
   ).length;
 
-   console.log("Final applicantCount:", applicantCount);
-   
+  console.log("Final applicantCount:", applicantCount);
 
-    return (
-        <div>
-            <CommonCard icon={<JobIcon/>} title={jobItem?.title}  footerContent = {
-                <Button  className="disabled:opacity-60 flex h-11 items-center justify-center px-5 bg-violet-600 text:white hover:bg-violet-700">
-                     {applicantCount} Applicants
-                </Button>
-            } />
+  return (
+    <div>
+      <CommonCard
+        icon={<JobIcon />}
+        title={jobItem?.title}
+        footerContent={
+          <Button
+            onClick={() => setShowApplicantsDrawer(true)}
+            className="disabled:opacity-55 flex h-11 items-center justify-center px-5 bg-violet-600 text:white hover:bg-violet-700"
+            disabled={
+              jobApplications.filter(
+                (item) => item.jobId === jobItem?._id.toString()
+              ).length === 0
+            }
+          >
+            {applicantCount} Applicants
+          </Button>
+        }
+      />
 
-            <JobApplications
-                showApplicantsDrawer={showApplicantsDrawer}
-                setShowApplicantsDrawer={setShowApplicantsDrawer}
-                showCurrentCandidateDetailsModal={showCurrentCandidateDetailsModal}
-                setShowCurrentCandidateDetailsModal={setShowCurrentCandidateDetailsModal}
-                currentCandidateDetails={currentCandidateDetails}
-                setCurrentCandidateDetails={setCurrentCandidateDetails}
-                jobItem={jobItem}
-                jobApplications={jobApplications.filter(jobApplicantItem.jobId === jobItem?._id)}
-            />
-        </div>
-    )
+      <JobApplications
+        showApplicantsDrawer={showApplicantsDrawer}
+        setShowApplicantsDrawer={setShowApplicantsDrawer}
+        showCurrentCandidateDetailsModal={showCurrentCandidateDetailsModal}
+        setShowCurrentCandidateDetailsModal={
+          setShowCurrentCandidateDetailsModal
+        }
+        currentCandidateDetails={currentCandidateDetails}
+        setCurrentCandidateDetails={setCurrentCandidateDetails}
+        jobItem={jobItem}
+        jobApplications={jobApplications.filter(
+          (jobApplicantItem) => jobApplicantItem.jobId === jobItem?._id
+        )}
+      />
+    </div>
+  );
 }
 
-export default RecruiterJobCard
-
+export default RecruiterJobCard;
