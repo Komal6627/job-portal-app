70e42d751146da2c8c3011549856bac6dac2ac6d
Komal6627 <komalkilledar1996@gmail.com>
2025-09-12 11:50:28 +0530

    view profile button working

--- src/components/candidate-list/index.js
+++ src/components/candidate-list/index.js
@@ -1,24 +1,58 @@
-'use client'
+"use client";
 
-import { Button } from "../ui/button"
+import { Button } from "../ui/button";
+import { Dialog, DialogContent,DialogTitle } from "../ui/dialog";
 
- 
-function CandidateList({jobApplications, currentCandidateDetails, setCurrentCandidateDetails, showCurrentCandidateDetailsModal, setShowCurrentCandidateDetailsModal}) {
-    return (
-        <>
-            <div className="grid grid-cols-1 gap-3 p-10 md:grid-cols-2 lg:grid-cols-3">
-                {
-                jobApplications && jobApplications.length > 0 ? 
-                 jobApplications.map(jobApplicantItem => <div    key={jobApplicantItem._id || jobApplicantItem.id}  className="bg-white shadow-lg w-full max-w-sm rounded-lg overflow-hidden mx-auto mt-4">
-                    <div className="px-4 my-6 flex justify-between items-center">
-                        <h3>{jobApplicantItem?.name}</h3>
-                        <Button className="disabled:opacity-60 flex h-11 items-center justify-center px-5 bg-violet-600 text:white hover:bg-violet-700">View Profile</Button>
-                    </div>
-                 </div>) : null
-                }
-            </div>
-        </>
-    )
+
+
+function CandidateList({
+  jobApplications,
+  currentCandidateDetails,
+  setCurrentCandidateDetails,
+  showCurrentCandidateDetailsModal,
+  setShowCurrentCandidateDetailsModal,
+}) {
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-3 p-10 md:grid-cols-2 lg:grid-cols-3">
+        {jobApplications && jobApplications.length > 0
+          ? jobApplications.map((jobApplicantItem) => (
+              <div
+                key={jobApplicantItem._id || jobApplicantItem.id}
+                className="bg-white shadow-lg w-full max-w-sm rounded-lg overflow-hidden mx-auto mt-4"
+              >
+                <div className="px-4 my-6 flex justify-between items-center">
+                  <h3>{jobApplicantItem?.name}</h3>
+                  <Button
+                    onClick={() => setShowCurrentCandidateDetailsModal(true)}
+                    className="disabled:opacity-60 flex h-11 items-center justify-center px-5 bg-violet-600 text:white hover:bg-violet-700"
+                  >
+                    View Profile
+                  </Button>
+                </div>
+              </div>
+            ))
+          : null}
+      </div>
+      <Dialog
+        open={showCurrentCandidateDetailsModal}
+        onOpenChange={setShowCurrentCandidateDetailsModal}
+      >
+               
+
+<DialogContent>
+  
+    <DialogTitle>Candidate Details</DialogTitle>
+  
+  <div className="mt-4">
+    {/* content */}
+  </div>
+</DialogContent>
+
+
+      </Dialog>
+    </>
+  );
 }
 
-export default CandidateList
+export default CandidateList;
